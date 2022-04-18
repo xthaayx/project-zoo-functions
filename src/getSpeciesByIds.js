08fcff7632a2234e0dcfcecb13bb1c56d899f54e
Thaysa <thay.fernnanda@gmail.com>
2022-04-18 17:20:34 -0300

    adiciona requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  const result = [];
+  for (let index = 0; index < ids.length; index += 1) {
+    species.filter((specie) => specie.id === ids[index]);
+    result.push(species[index]);
+  }
+  if (ids.length === 1) {
+    return species.filter((specie) => specie.id === ids[0]);
+  }
+  return result;
 }
-
 module.exports = getSpeciesByIds;
