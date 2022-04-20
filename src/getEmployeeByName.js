b8679b5e67a2ee44ffe756e51e28d920c6971e1b
Thaysa <thay.fernnanda@gmail.com>
2022-04-20 13:53:37 -0300

    adiciona requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  return employees.find((employee) => (
+    employee.firstName === employeeName || employee.lastName === employeeName
+  ));
 }
 
 module.exports = getEmployeeByName;
