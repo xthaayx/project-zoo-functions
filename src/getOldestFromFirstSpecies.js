72e636fe3f3cc0a3573f950bd73f984e0d4b9390
Thaysa <thay.fernnanda@gmail.com>
2022-04-23 12:33:26 -0300

    adiciona requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
+const { species, employees } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const firstEmploy = employees.find((employee) => employee.id === id).responsibleFor[0];
+  const firstSpecie = species.find((specie) => specie.id === firstEmploy).residents;
+  return Object.values(firstSpecie.reduce((acumulador, valor) => {
+    if (acumulador.age < valor.age) {
+      return valor;
+    } return acumulador;
+  }));
 }
 
 module.exports = getOldestFromFirstSpecies;
