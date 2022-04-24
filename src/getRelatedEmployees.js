db73f4a169c86caafecd104ca5ea0911e81e2709
Thaysa <thay.fernnanda@gmail.com>
2022-04-24 11:09:54 -0300

    adiciona solução do lint

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,5 +1,6 @@
 const data = require('../data/zoo_data');
 const { employees } = require('../data/zoo_data');
+
 function isManager(id) {
   // seu código aqui
   return employees.some((employee) => employee.managers.includes(id));
