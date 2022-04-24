4a9f5cbd5ec98d5544c6614a143dab7feedfd17a
Thaysa <thay.fernnanda@gmail.com>
2022-04-24 11:06:07 -0300

    adiciona requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,19 @@
 const data = require('../data/zoo_data');
-
+const { employees } = require('../data/zoo_data');
 function isManager(id) {
   // seu código aqui
+  return employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } else {
+    const employeeList = employees.filter((employee) =>
+      employee.managers.includes(managerId) === true);
+    return employeeList.map((employee) => `${employee.firstName} ${employee.lastName}`);
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
